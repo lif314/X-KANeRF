7f4aefd6daf36980ba61388ef55edc513ebf772e
lif314 <lifer314@163.com>
2024-05-11 09:09:14 +0800

    update fourier-kan

--- xKANeRF/xKAN/fourier_kan.py
+++ xKANeRF/xKAN/fourier_kan.py
@@ -1,5 +1,23 @@
-import torch as th
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
+from typing import *
+
+class FourierBasisFunction(nn.Module):
+    def __init__(
+        self,
+        grid_min: float = -2.,
+        grid_max: float = 2.,
+        grid_size: int = 8,  
+    ):
+        super().__init__()
+        grid = torch.linspace(grid_min, grid_max, grid_size)
+        self.grid = torch.nn.Parameter(grid, requires_grad=False)
+
+    def forward(self, x):
+        return torch.exp(-(x[..., None] - self.grid) ** 2)
+        pass
 
 #This is inspired by Kolmogorov-Arnold Networks but using 1d fourier coefficients instead of splines coefficients
 #It should be easier to optimize as fourier are more dense than spline (global vs local)
@@ -7,9 +25,9 @@ import numpy as np
 #The other advantage of using fourier over spline is that the function are periodic, and therefore more numerically bounded
 #Avoiding the issues of going out of grid
 
-class NaiveFourierKANLayer(th.nn.Module):
+class FourierKANLayer(nn.Module):
     def __init__( self, inputdim, outdim, gridsize,addbias=True):
-        super(NaiveFourierKANLayer,self).__init__()
+        super(FourierKANLayer,self).__init__()
         self.gridsize= gridsize
         self.addbias = addbias
         self.inputdim = inputdim
@@ -18,26 +36,26 @@ class NaiveFourierKANLayer(th.nn.Module):
         #The normalization has been chosen so that if given inputs where each coordinate is of unit variance,
         #then each coordinates of the output is of unit variance 
         #independently of the various sizes
-        self.fouriercoeffs = th.nn.Parameter( th.randn(2,outdim,inputdim,gridsize) / 
-                                             (np.sqrt(inputdim) * np.sqrt(self.gridsize) ) )
+        self.fouriercoeffs = torch.nn.Parameter(torch.randn(2,outdim,inputdim,gridsize) / 
+                                             (torch.sqrt(inputdim) * np.sqrt(self.gridsize) ) )
         if( self.addbias ):
-            self.bias  = th.nn.Parameter( th.zeros(1,outdim))
+            self.bias  = torch.nn.Parameter(torch.zeros(1,outdim))
 
     #x.shape ( ... , indim ) 
     #out.shape ( ..., outdim)
     def forward(self,x):
         xshp = x.shape
         outshape = xshp[0:-1]+(self.outdim,)
-        x = th.reshape(x,(-1,self.inputdim))
+        x = torch.reshape(x,(-1,self.inputdim))
         #Starting at 1 because constant terms are in the bias
-        k = th.reshape( th.arange(1,self.gridsize+1,device=x.device),(1,1,1,self.gridsize))
-        xrshp = th.reshape(x,(x.shape[0],1,x.shape[1],1) ) 
+        k = torch.reshape(torch.arange(1,self.gridsize+1,device=x.device),(1,1,1,self.gridsize))
+        xrshp = torch.reshape(x,(x.shape[0],1,x.shape[1],1) ) 
         #This should be fused to avoid materializing memory
-        c = th.cos( k*xrshp )
-        s = th.sin( k*xrshp )
+        c = torch.cos( k*xrshp )
+        s = torch.sin( k*xrshp )
         #We compute the interpolation of the various functions defined by their fourier coefficient for each input coordinates and we sum them 
-        y =  th.sum( c*self.fouriercoeffs[0:1],(-2,-1)) 
-        y += th.sum( s*self.fouriercoeffs[1:2],(-2,-1))
+        y =  torch.sum( c*self.fouriercoeffs[0:1],(-2,-1)) 
+        y += torch.sum( s*self.fouriercoeffs[1:2],(-2,-1))
         if( self.addbias):
             y += self.bias
         #End fuse
@@ -54,66 +72,28 @@ class NaiveFourierKANLayer(th.nn.Module):
         print("diff")
         print(diff) #should be ~0
         '''
-        y = th.reshape( y, outshape)
+        y = torch.reshape( y, outshape)
         return y
 
-def demo():
-    bs = 10
-    L = 3 #Not necessary just to show that additional dimensions are batched like Linear
-    inputdim = 50
-    hidden = 200
-    outdim = 100
-    gridsize = 300
+class Fourier_KAN(nn.Module):
+    def __init__(
+        self,
+        layers_hidden: List[int],
+        grid_min: float = -2.,
+        grid_max: float = 2.,
+        grid_size: int = 8,
+    ) -> None:
+        super().__init__()
+        self.layers = nn.ModuleList([
+            FourierKANLayer(
+                in_dim, out_dim,
+                grid_min=grid_min,
+                grid_max=grid_max,
+                gridsize=grid_size,
+            ) for in_dim, out_dim in zip(layers_hidden[:-1], layers_hidden[1:])
+        ])
 
-    device = "cpu" #"cuda"
-
-    fkan1 = NaiveFourierKANLayer(inputdim, hidden, gridsize).to(device)
-    fkan2 = NaiveFourierKANLayer(hidden, outdim, gridsize).to(device)
-
-    x0 =th.randn(bs,inputdim).to(device)
-
-    h = fkan1(x0)
-    y = fkan2(h)
-    print("x0.shape")
-    print( x0.shape)
-    print("h.shape")
-    print( h.shape)
-    print( "th.mean( h )")
-    print( th.mean( h ) )
-    print( "th.mean( th.var(h,-1) )")
-    print( th.mean( th.var(h,-1)))
-
-    print("y.shape")
-    print( y.shape )
-    print( "th.mean( y)")
-    print( th.mean( y ) )
-    print( "th.mean( th.var(y,-1) )")
-    print( th.mean( th.var(y,-1)))
-
-    print(" ")
-    print(" ")
-    print("Sequence example")
-    print(" ")
-    print(" ")
-    xseq =th.randn(bs, L ,inputdim).to(device)
-
-    h = fkan1(xseq)
-    y = fkan2(h)
-    print("xseq.shape")
-    print( xseq.shape)
-    print("h.shape")
-    print( h.shape)
-    print( "th.mean( h )")
-    print( th.mean( h ) )
-    print( "th.mean( th.var(h,-1) )")
-    print( th.mean( th.var(h,-1)))
-
-    print("y.shape")
-    print( y.shape )
-    print( "th.mean( y)")
-    print( th.mean( y ) )
-    print( "th.mean( th.var(y,-1) )")
-    print( th.mean( th.var(y,-1)))
-
-if __name__ == "__main__":
-    demo()
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
