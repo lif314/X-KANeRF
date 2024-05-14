a6115db36e01248cc17a884a735f3a0def85105e
lif314 <cslinfeili@gmail.com>
2024-05-14 13:56:50 +0800

    add render scripts

--- run_render.sh
+++ run_render.sh
@@ -6,11 +6,24 @@ echo "Start Time: $start_time"
 start_timestamp=$(date -d "$start_time" +%s)
 
 # export CUDA_VISIBLE_DEVICES=1
-
+# Render example
 # exp_path=outputs/rbf-blender-lego/xkanerf/2024-05-11_162426
+
 exp_path=$1
-ns-eval --load-config $exp_path/config.yml \
-        --output-path $exp_path/eval.json
+
+# Render rgb
+ns-render interpolate  \
+    --load-config $exp_path/config.yml \
+    --output-path $exp_path/renders/rgb.mp4 \
+    --rendered-output-names rgb \
+    --interpolation-steps 1
+
+# Render depth
+ns-render interpolate  \
+    --load-config $exp_path/config.yml \
+    --output-path $exp_path/renders/depth.mp4 \
+    --rendered-output-names depth \
+    --interpolation-steps 1
 
 
 end_time=$(date +"%Y-%m-%d %H:%M:%S")
